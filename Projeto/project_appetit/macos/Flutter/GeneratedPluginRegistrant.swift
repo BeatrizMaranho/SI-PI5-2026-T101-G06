41b4785eb3c7aa3fb747d1babc79ebcb224c5bef
Matias Amma <matiasamma@gmail.com>
2026-04-24 17:09:24 -0300

    fix: merge para atualizar dependências e corrigir imports

--- Projeto/project_appetit/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Projeto/project_appetit/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,17 +7,17 @@ import Foundation
 
 import cloud_firestore
 import file_selector_macos
+import firebase_app_check
 import firebase_auth
 import firebase_core
-import firebase_app_check
 import firebase_storage
 import shared_preferences_foundation
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FirebaseAppCheckPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
